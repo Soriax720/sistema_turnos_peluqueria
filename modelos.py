cb73a9592fb5ab85bd8a5cd925ecb5a52eb1c061
Soriax720 <julisoria744@gmail.com>
2025-11-11 21:55:31 -0300

    Completado Turno,incluido get_datos

--- modelos.py
+++ modelos.py
@@ -1,10 +1,7 @@
-
+from datetime import datetime, timedelta
 from abc import ABC, abstractmethod 
 
-"""class TipoServicio(Enum):
-    CORTE = "Corte"
-    COLOR = "Color"
-    BARBA = "Barba" """
+
 TIPOS_SERVICIO = ["Corte", "Color", "Barba"]
 
 class Persona(ABC):
@@ -12,19 +9,18 @@ class Persona(ABC):
         self.nombre = nombre
         self.apellido = apellido
         self.telefono = telefono
+
     @abstractmethod
     def mostrar_info(self):
         pass
 
 class Peluquero(Persona):
 
-    
-
     def __init__(self, nombre, apellido, telefono,id_peluquero,especialidad):
         super().__init__(nombre, apellido, telefono)
         self.id_peluquero = id_peluquero
-        if especialidad not in Peluquero.TIPOS_SERVICIO:
-            raise ValueError(f"Especialidad no valida: {especialidad}, Validas: {Peluquero.TIPOS_SERVICIO}")
+        if especialidad not in TIPOS_SERVICIO:
+            raise ValueError(f"Especialidad no valida: {especialidad}, Validas: {TIPOS_SERVICIO}")
 
         self.especialidad = especialidad
 
@@ -61,3 +57,43 @@ class Cliente(Persona):
             "apellido": self.apellido,
             "telefono": self.telefono
         }
+
+class Turno:
+    def __init__(self,cliente,peluquero,id_turno,servicio,fecha_hora):
+        if servicio not in TIPOS_SERVICIO:
+            raise ValueError(f"El servicio '{servicio}' no es valido. Servicios validos: {TIPOS_SERVICIO}")
+
+        self.cliente = cliente
+        self.peluquero = peluquero
+        self.id_turno = id_turno
+        self.servicio = servicio
+        self.fecha_hora = fecha_hora
+        
+    def mostrar_turno(self):
+        formato_lectura = "%d/%m/%Y a las %H:%M"
+        fecha_formateada = self.fecha_hora.strftime(formato_lectura)
+        reporte = (
+            f"\n=== DETALLE DEL TURNO #{self.id_turno} === \n"
+            f"Servicio: {self.servicio} \n"
+            f"Fecha y Hora: {fecha_formateada}\n"
+            f"---------------------------------\n"
+            f"Datos del Cliente: {self.cliente.mostrar_info()}\n"
+            f"Datos Del Peluquero: {self.peluquero.mostrar_info()}\n"
+            f"=========================================\n"
+        )
+        return reporte
+    
+    def get_datos(self):
+        formato_lectura = "%Y-%m-%d %H:%M"
+        fecha_formateada = self.fecha_hora.strftime(formato_lectura)
+        return{
+            "id_turno": self.id_turno,
+            "fecha_hora": fecha_formateada,
+            "cliente_id": self.cliente.id_cliente,
+            "peluquero_id": self.peluquero.id_peluquero,
+            "servicio": self.servicio
+
+        }
+
+
+
