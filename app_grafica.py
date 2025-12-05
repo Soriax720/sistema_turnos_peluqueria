a5c39af2db5ee0929e01bc3f06a0a75bca7a9591
Soriax720 <julisoria744@gmail.com>
2025-12-05 00:21:20 -0300

    Conexión frontend-backend y lógica completa de Registrar Cliente

--- app_grafica.py
+++ app_grafica.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox # ventanas emergentes de alerta
 from gestor_turnos import GestorTurnos
 
 class SalonApp:
@@ -25,7 +26,7 @@ class SalonApp:
 
         self.var_nombre = tk.StringVar()
         self.var_apellido = tk.StringVar()
-        self.var_telofono = tk.StringVar()
+        self.var_telefono = tk.StringVar()
 
         #campo nombre
         tk.Label(self.ventana_registro, text="Nombre:").pack(padx=5)
@@ -35,15 +36,31 @@ class SalonApp:
         tk.Entry(self.ventana_registro, textvariable= self.var_apellido).pack(pady=5)
         #campo telefono
         tk.Label(self.ventana_registro, text="Telefono:").pack(pady=5)
-        tk.Entry(self.ventana_registro, textvariable= self.var_telofono).pack(pady=5)
+        tk.Entry(self.ventana_registro, textvariable= self.var_telefono).pack(pady=5)
 
         btn_guardar = tk.Button(self.ventana_registro, text="Guardar Cliente", command= self.accion_guardar_cliente)
         btn_guardar.pack(pady=20)
     
     def accion_guardar_cliente(self):
-        
-        print("Botón presionado. Implementar lógica aquí.")
-    
+        nombre = self.var_nombre.get().strip()
+        apellido = self.var_apellido.get().strip()
+        telefono = self.var_telefono.get().strip()
+
+        if not nombre or not apellido or not telefono:
+            messagebox.showwarning("Faltan Datos", "Por favor complete todos los campos.")
+            return
+
+        #llamada al gestor
+        try:
+            nuevo_id = self.gestor.registrar_cliente(nombre, apellido, telefono)
+            messagebox.showinfo("Éxito", f"Cliente registrado correctamente.\nID Asignado: {nuevo_id}")
+            self.ventana_registro.destroy()
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo registrar el cliente.\nDetalle: {e}")
+
+
+
+
 if __name__ == "__main__":
     root = tk.Tk()       # Crea la ventana base
     app = SalonApp(root) # Inicia tu App dentro de esa ventana
